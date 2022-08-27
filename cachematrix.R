aad950f036eb4392822c972d9c0c7a74791e83ab
UlshaRS <110580198+UlshaRS@users.noreply.github.com>
2022-08-27 17:46:39 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A) Purpose: Create a low resource data cache for use by a repeated request
+## B) Function: Two functions set within an enclosed environment, subset
+## from the global environment, to create and set a matrix, invert (solve(x))
+## the matrix, do a save state query and retrieve (with message) if cache is
+## detected or, if no cache is detected, and create one for reference access
+## by global environment, without needing to create the enclosed environment
+## each time the functions are needed.
 
-## Write a short comment describing this function
+## Get, set, invert matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function() inv <<- solve(x)
+  getInverse <- function() inv
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Check cache save state (!NULL=load, message) || (NULL=save)
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if (!is.null(inv)) {
+    message ("Fetching")
+    return(inv)
+  }
+  data <- X$get()
+  inv <- solve(data, ...)
+  x$setInverse(inv)
+  inv
 }
